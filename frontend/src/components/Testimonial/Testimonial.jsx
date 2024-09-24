a348745cc6d6ccd0e9dbcba404a3e02080529487
Javeedm07 <114037880+Javeedm07@users.noreply.github.com>
2024-09-24 21:22:40 +0530

    Update Testimonial.jsx

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import 'swiper/swiper-bundle.min.css';
 import "swiper/css";
 import "swiper/css/pagination";
 import patientAvatar from "../../assets/images/patient-avatar.png";
