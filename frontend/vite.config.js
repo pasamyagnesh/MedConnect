54a166ebb88bc45b8c9a75023778d2d6562da0c7
Javeedm07 <114037880+Javeedm07@users.noreply.github.com>
2024-09-24 22:48:22 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src', // Update if you use custom aliases
-      swiper: './node_modules/swiper/react',
+      swiper: 'node_modules/swiper/react',
     },
   },
   build: {
